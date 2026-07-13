da68a13dd48b1b306f2b42657c89fb05c13793d4
laurenpyne <lauren.pyne@proton.com>
2026-07-13 10:09:36 +1000

    Move configuration into host layout

--- flake.nix
+++ flake.nix
@@ -1,16 +1,18 @@
 {
+  description = "Cozy Config";
+
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
   outputs = inputs: {
     nixosConfigurations.Cozy = inputs.nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
+
       modules = [
         { nix.settings.experimental-features = [ "nix-command" "flakes" ]; }
-        ./configuration.nix
+        ./hosts/Cozy/configuration.nix
       ];
     };
   };
 }
-
