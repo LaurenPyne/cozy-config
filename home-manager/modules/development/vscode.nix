b69711ccc2558eac533030dd5b62a520d30267cb
laurenpyne <lauren.pyne@proton.com>
2026-07-14 11:19:29 +1000

    Configure VS Code and direnv development environment

--- home-manager/modules/development/vscode.nix
+++ home-manager/modules/development/vscode.nix
@@ -1,7 +1,31 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
+  home.packages = with pkgs; [
+    nixd
+    nixfmt-rfc-style
+  ];
+
   programs.vscode = {
     enable = true;
+
+    profiles.default = {
+      userSettings = {
+        "workbench.colorTheme" = "Everforest Pro Dark Vibrant";
+        "terminal.integrated.initialHint" = false;
+
+        "[nix]" = {
+          "editor.defaultFormatter" = "jnoortheen.nix-ide";
+          "editor.formatOnSave" = true;
+        };
+
+        "nix.enableLanguageServer" = true;
+        "nix.serverPath" = "nixd";
+      };
+      extensions = with pkgs.vscode-extensions; [
+      jnoortheen.nix-ide
+      mkhl.direnv
+       ];
+    };
   };
 }
