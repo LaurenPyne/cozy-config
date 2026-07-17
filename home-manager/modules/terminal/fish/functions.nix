aa7a87798c460e563bc1537d5c630978982e5ea8
laurenpyne <lauren.pyne@proton.com>
2026-07-17 11:12:50 +1000

    Polished fish

--- home-manager/modules/terminal/fish/functions.nix
+++ home-manager/modules/terminal/fish/functions.nix
@@ -2,10 +2,48 @@ _:
 
 {
   programs.fish.functions = {
+
+    # Cozy
+    cfg = ''
+      cd ~/cozy-config
+      and git status
+    '';
+
     rebuild = ''
       just pre-commit
       and just build
       and just switch
     '';
+
+    update = ''
+      nix flake update
+      and just pre-commit
+      and just build
+    '';
+
+    clean = ''
+      nix store gc
+      and nix-collect-garbage -d
+    '';
+
+    # Filesystem
+    mkcd = ''
+      mkdir -p $argv
+      and cd $argv
+    '';
+
+    # Future
+
+    # extract = ''
+    #   # Extract any supported archive type
+    # '';
+
+    # weather = ''
+    #   curl wttr.in
+    # '';
+
+    # ip = ''
+    #   curl ifconfig.me
+    # '';
   };
 }
